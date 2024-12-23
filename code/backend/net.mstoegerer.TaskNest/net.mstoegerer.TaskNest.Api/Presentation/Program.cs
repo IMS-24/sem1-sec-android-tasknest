57cfcd1791418cb08e0d51e92e635a5f82c43550
Markus stoegerer <markus.stoegerer@gmail.com>
2024-12-23 15:51:06 +0100

    test [ci / cd]

--- code/backend/net.mstoegerer.TaskNest/net.mstoegerer.TaskNest.Api/Presentation/Program.cs
+++ code/backend/net.mstoegerer.TaskNest/net.mstoegerer.TaskNest.Api/Presentation/Program.cs
@@ -22,11 +22,9 @@ if (seed)
 
 var builder = WebApplication.CreateBuilder(args);
 var configurationBuilder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("Presentation/appsettings.Development.json", true)
-        .AddJsonFile("Presentation/appsettings.Production.json", true)
-        .AddEnvironmentVariables()
-    ;
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("Presentation/appsettings.Development.json", true)
+    .AddJsonFile("Presentation/appsettings.Production.json", true);
 
 IConfiguration configuration = configurationBuilder
     .Build();
